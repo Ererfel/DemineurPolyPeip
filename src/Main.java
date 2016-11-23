ad903e5a9bbc886249f2c08b3209e59421582723
Gribsy <gregoire97.ggp@gmail.com>
2016-11-23 15:36:05 +0100

    Importation du travail de michael comme une première base

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
-/**
- * Created by user on 17/11/16.
- */
+import javax.swing.JFrame;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("je suis un joyeux démineur");
+    public static void main(String[] args){
+        int t=0;
+        int tab[][] = new int[10][10];
+        for (int i = 0; i <10 ; i++){
+            for (int j = 0; j<10 ; j++){
+                tab[i][j]=t;
+                t++;
+            }
+        }
+        System.out.print(Arrays.deepToString(tab));
     }
 }
